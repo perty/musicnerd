77436721f9dca4c3dcf038b7758aeec08e806467
Per Lundholm <per@artcomputer.se>
2022-07-26 15:12:57 +0200

    Converted response to record.

--- src/main/java/se/artcomputer/demo/musicnerd/MusicNerdResponse.java
+++ src/main/java/se/artcomputer/demo/musicnerd/MusicNerdResponse.java
@@ -4,57 +4,12 @@ import se.artcomputer.demo.musicnerd.musicbrainz.*;
 
 import java.util.List;
 
-public final class MusicNerdResponse {
-    private final String mbid;
-    private final String name;
-    private final String gender;
-    private final String disambiguation;
-    private final String country;
-    private final Description description;
-    private final List<Album> albums;
-
-    public MusicNerdResponse(
-            MusicBrainzId mbid,
-            ArtistName name,
-            ArtistGender artistGender,
-            ArtistDisambiguation artistDisambiguation,
-            ArtistCountry artistCountry,
-            Description description,
-            List<Album> albums) {
-        this.mbid = mbid.mbid();
-        this.name = name.name();
-        this.gender = artistGender.gender();
-        this.disambiguation = artistDisambiguation.disambiguation();
-        this.country = artistCountry.country();
-        this.description = description;
-        this.albums = albums;
-    }
-
-    public String getMbid() {
-        return mbid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public String getDisambiguation() {
-        return disambiguation;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public Description getDescription() {
-        return description;
-    }
-
-    public List<Album> getAlbums() {
-        return albums;
-    }
+public record MusicNerdResponse(
+        MusicBrainzId mbid,
+        ArtistName name,
+        ArtistGender artistGender,
+        ArtistDisambiguation artistDisambiguation,
+        ArtistCountry artistCountry,
+        Description description,
+        List<Album> albums) {
 }
